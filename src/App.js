9381732ad16b377ccee21ee4a23dc99f1b545c31
sisoalbert <sisoalbert@yahoo.com>
2022-09-03 11:42:32 +0200

    fetching from API Working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
 // import logo from "./logo.svg";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Header from "./components/Header";
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch("https://reactnative.dev/movies.json");
+      const json = await response.json();
+      console.log("====================================");
+      console.log(json);
+      console.log("====================================");
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
-      App
+      <Header />
+      {loading ? (
+        <>
+          <h1
+            style={{
+              color: "red",
+            }}
+          >
+            Loading...
+          </h1>
+        </>
+      ) : (
+        <>
+          <h1
+            style={{
+              color: "green",
+            }}
+          >
+            Done loading
+          </h1>
+          <>
+            {data.map((item) => {
+              return (
+                <div>
+                  <h1
+                    style={{
+                      color: "coral",
+                    }}
+                  >
+                    {item.title}
+                    <div>{item.releaseYear}</div>
+                  </h1>
+                  <></>
+                </div>
+              );
+            })}
+          </>
+        </>
+      )}
+
       <></>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,48 @@
 import React from "react";
 
+// import logo from "../logo.svg";
+import logo from "../assets/bookslogo.svg";
+import "../styles/Header.css";
+
 export default function Header() {
   return (
-    <div>
-      <div>
-        <h1>Logo</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: "black",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingLeft: "10px",
+        paddingRight: "10px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          //   backgroundColor: "yellow",
+        }}
+      >
+        <div
+          style={{
+            // backgroundColor: "yellow",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={logo} alt="logo" />
+        </div>
+        <h1>React js Books</h1>
       </div>
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          //   backgroundColor: "green",
+        }}
+      >
         <div>
           <h1>Home</h1>
         </div>
