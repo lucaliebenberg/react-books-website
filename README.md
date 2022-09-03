ab2c4d732b08c3345516589d95ec6bf6cb890ec8
sisoalbert <sisoalbert@yahoo.com>
2022-09-03 10:35:13 +0200

    working on the header component

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-books-website
+
 This was a Reactjs project which encapsulates creating a book website
+
+Step 1:
+#git clone PATH/....git
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <div>
+        <h1>Logo</h1>
+      </div>
+      <div>
+        <div>
+          <h1>Home</h1>
+        </div>
+        <div>
+          <h1>Contacts</h1>
+        </div>
+        <div>
+          <h1>About</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
