503919c1242869eff3e0b248bab2bfed610b85c6
sisoalbert <sisoalbert@yahoo.com>
2022-09-17 11:46:08 +0200

    Improved react route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import * as React from "react";
 import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import Contacts from "./pages/Contacts";
+import About from "./pages/About";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div>
+      {/* <Header /> */}
+
       {/* Routes nest inside one another. Nested route paths build upon
             parent route paths, and nested route elements render inside
             parent route elements. See the note about <Outlet> below. */}
@@ -12,7 +19,10 @@ export default function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
-            <Route path="dashboard" element={<Dashboard />} />
+
+            <Route path="contacts" element={<Contacts />} />
+            <Route path="about" element={<About />} />
+            <Route path="details" element={<Details />} />
 
             {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
@@ -30,53 +40,11 @@ function Layout() {
     <div>
       {/* A "layout route" is a good place to put markup you want to
           share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
+      <Header />
       {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
       <Outlet />
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
     </div>
   );
 }
